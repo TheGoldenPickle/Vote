c2a80a32b34d6a7734959e4b04cc074170ab2e90
SavionLegendZzz <saviondeavault@gmail.com>
2015-11-05 18:25:21 -0500

    Task!

---
+++ src/Savion/task.php
@@ -0,0 +1,26 @@
+<?php
+namespace Savion;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\plugin\Plugin;
+use pocketmine\Player;
+
+class task extends PluginTask{
+	public $player;
+	public function __construct(Plugin $owner, Player $player){
+		parent::__construct($owner);
+		$this->player = $player;
+	}
+	
+	public function onRun($currentTick){
+		if ($this->player instanceof Player){
+			if (in_array($this->player->getName(), $this->getOwner()->voters)){
+				$voters = array_search($this->player->getName(), $this->getOwner()->voters);
+				unset($this->getOwner()->voters[$id]);
+				$this->getOwner()->no = 0;
+				$this->getOnwer()->yes = 0;
+				
+			}
+		}
+	}
+}
